d6a9c281dd7a0c708c71d3d2d967daba649c1494
Jack Shen <op_chieh@hotmail.com>
2020-08-25 17:20:14 +0800

    2020/08/25

---
+++ Project/my-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <products></products>
+  `
+})
+export class AppComponent {
+
+  onClickMe($event){
+    console.log("Clicked",$event)
+  }
+}
